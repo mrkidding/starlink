ceea6fe796f19a8a8cf47ab9c509c30a57dd9d60
mrkidding <41988918+mrkidding@users.noreply.github.com>
2020-08-13 22:04:10 -0400

    Starlink

---
+++ src/components/Main.js
@@ -0,0 +1,84 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+import SatSetting from './SatSetting';
+import SatelliteList from './SatelliteList';
+import WorldMap from './WorldMap';
+import {NEARBY_SATELLITE, SAT_API_KEY, STARLINK_CATEGORY} from "./constants";
+
+
+class Main extends Component {
+    constructor(){
+        super();
+        this.state = {
+            satInfo: null,
+            settings: null,
+            satList: null,
+            isLoadingList: false
+        };
+    }
+
+    showNearbySatellite = (setting) => {
+        this.setState({
+            settings: setting,
+            isLoadingList: true
+        })
+        console.log('setting-> ', setting);
+        this.fetchSatellite(setting);
+    }
+
+    fetchSatellite= (setting) => {
+
+        const {observerLat, observerLong, observerElevation, satAlt} = setting;
+        const url = `${NEARBY_SATELLITE}/${observerLat}/${observerLong}/${observerElevation}/${satAlt}/${STARLINK_CATEGORY}/&apiKey=${SAT_API_KEY}`;
+
+        this.setState({
+            isLoadingList: true
+        });
+
+        axios.get(url)
+            .then(response => {
+                console.log(response.data)
+                this.setState({
+                    satInfo: response.data,
+                    isLoadingList: false
+                })
+            })
+            .catch(error => {
+                console.log('err in fetch satellite -> ', error);
+            })
+    }
+
+    showMap = (selected) => {
+        console.log('show selected -> ', selected);
+        const satList = selected;
+        console.log('satList -> ', [...satList]);
+        this.setState(preState => ({
+            ...preState,
+            satList: [...selected]
+
+        }))
+
+
+    }
+
+    render() {
+        const { satInfo } = this.state;
+        return (
+            <div className='main'>
+                <div className="left-side">
+                    <SatSetting onShow={this.showNearbySatellite}/>
+                    <SatelliteList satInfo={satInfo}
+                                   isLoad={this.state.isLoadingList}
+                                   onShowMap={this.showMap}
+                    />
+                </div>
+                <div className="right-side">
+                    <WorldMap satData={this.state.satList}
+                              observerData={this.state.settings}/>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Main;
